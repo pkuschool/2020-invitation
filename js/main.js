70e429ff2c33566fed6aa2835030b97d577ba002
steven12138 <18611209860@163.com>
2020-06-26 20:42:15 +0800

    try cutting image

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function submit() {
     var name_el = document.querySelector('#stacked-name');
     var house_el = document.querySelector('#stacked-house');
@@ -9,7 +8,7 @@ function submit() {
     if (name == "" || house == "请选择书院") {
         alert("信息填写不完整");
         return
-    }else{
+    } else {
         localStorage.setItem("name", name);
         localStorage.setItem("house", house);
         console.log(preview_el.src.length)
@@ -21,8 +20,8 @@ function submit() {
             // var avatar = avatar_el.files[0];
             // var reader = new FileReader();
             // reader.onload = function (e) {
-                //     localStorage.avatar=e.target.result;
-                //     window.open('./print.html', '_self')
+            //     localStorage.avatar=e.target.result;
+            //     window.open('./print.html', '_self')
             //     // preview_el.src = e.target.result;
             // }
             reader.readAsDataURL(avatar);
@@ -33,21 +32,53 @@ function submit() {
         // window.location = "./print.html";
     }
 }
-window.onload = () => {
-    // document.body.querySelector("#form").style.height = window.innerHeight + "px";
+
+function preview(c) {
+    console.log(c);
 }
 
+
+var jcrop_api;
 document.querySelector('#input-file').onchange = () => {
     var reader = new FileReader();
     reader.onload = function (e) {
         document.querySelector('#avatar-img').src = e.target.result
         document.querySelector('#avatar-img').removeAttribute('hidden')
+        if (jcrop_api) {
+            jcrop_api.destroy();
+        }
+        document.querySelector('#target').src = e.target.result;
+        document.querySelector('#preview').src = e.target.result;
         if (e.target.result.length >= 4750000) {
             alert('图片过大，可能无法正确生成！')
+        }
+        document.querySelector('.preview-area').removeAttribute('hidden');
+        document.querySelector('.shadow').removeAttribute('hidden');
+        document.querySelector('#target').onload = () => {
+            jcrop_api = $.Jcrop('#target', {
+                onChange: preview,
+                onSelect: preview,
+
+                aspectRatio: 1 / 1
+            });
         }
         // document.querySelector('#filename-indicator').innerHTML = document.querySelector('#input-file').files[0].name
         // localStorage.setItem('avatar', e.target.result);
 
     }
     reader.readAsDataURL(document.querySelector('#input-file').files[0]);
+}
+
+function cut_confirm() {
+
+}
+
+function cut_cancel() {
+    jcrop_api.destroy();
+    document.querySelector('#target').style="";
+    document.querySelector('#input-file').value="";
+    document.querySelector('#avatar-img').setAttribute('hidden', '');
+    document.querySelector('#avatar-img').src = "#";
+    document.querySelector('.shadow').setAttribute('hidden', '');
+    document.querySelector('.preview-area').setAttribute('hidden', '');
 }
